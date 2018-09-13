af733b54ef8cb5699f8d71fc648b8d5032d6da96
Rohan <fastbiker703@gmail.com>
2018-09-12 20:44:27 -0400

    First program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//**************************************
+// Rohan Putcha        9 / 7 / 2018
+// HelloWorld.java
+// Prints "Hello, World!"
+
+public class HelloWorld {
+  public static void main(String [] args) {
+    System.out.println("Hello, World!");
+  }
+}
