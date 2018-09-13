97a558c4a1aeb3b79abf147aecf5595f29efa324
Rohan <fastbiker703@gmail.com>
2018-09-12 21:08:57 -0400

    Finished last program

---
+++ Initials.java
@@ -0,0 +1,20 @@
+// ******************************************************
+// Rohan Putcha
+//
+// Initials.java
+// ******************************************************
+
+public class Initials {
+  public static void main(String [] args) {
+    System.out.println("RRRRRRRRRR        PPPPPPPPPP");
+    System.out.println("RRRRRRRRRRR       PPPPPPPPPPP");
+    System.out.println("RR       RRR      PP       PPP");
+    System.out.println("RR       RRR      PP       PPP");
+    System.out.println("RR       RR       PP       PP");
+    System.out.println("RRRRRRRRRR        PPPPPPPPPP");
+    System.out.println("RRRRR             PP");
+    System.out.println("RR  RRR           PP");
+    System.out.println("RR    RRR         PP");
+    System.out.println("RR     RRRR       PP");
+  }
+}
