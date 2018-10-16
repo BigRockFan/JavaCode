3b2e18af6502abb3921d8ae50c7976de1f495cac
Rohan <fastbiker703@gmail.com>
2018-10-15 21:23:12 -0400

    testing for a question from a cs test

--- Test.java
+++ Test.java
@@ -1,25 +1,10 @@
+
 import java.util.Scanner;
 public class Test {
 public static void main(String [] args) {
-
-String color, name;
-int age, day;
-Scanner scan = new Scanner(System.in);
-System.out.print("Enter your favorite color: ");
-color = scan.nextLine();
-System.out.print("Enter your last name: ");
-name = scan.nextLine();
-System.out.print("Enter your age: ");
-age = scan.nextInt();
-System.out.print("Enter the day of the month you were born: ");
-day = scan.nextInt();
-int rando = (int) (Math.random() * 400 + 100);
-
-int lengthOfName = name.length();
-int lastnum = (int)(Math.sqrt(day + age));
-String ans = rando + (color.toLowerCase()) + ((name.substring(0, 1)).toUpperCase()) + ((name.substring(lengthOfName-1, lengthOfName)).toUpperCase()) + lastnum;
-
-System.out.println("Your secret password is: "+ans);
-
+int x= 5;
+int y = 10;
+String s = " Final";
+System.out.println(x+y+s);
 }
 }
