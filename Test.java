36e807aee716b3471a336d60952169081407bf31
Rohan <fastbiker703@gmail.com>
2018-10-23 23:27:35 -0400

    euler 3

--- Test.java
+++ Test.java
@@ -1,12 +1,18 @@
 
-import java.util.Scanner;
 public class Test {
-public static void main(String [] args) {
-int x= 5;
-int y = 10;
-int max = (x > y) ? x : y; //Ternary Operator Test
-System.out.println(max);
+    public static void main(String [] args) {
+        long total = 600851475143L; //600851475143
 
 
-}
+        for (long i = 2; i <= total/2;) {
+            if (total % i == 0) {
+                total = total / i;
+                System.out.println(i);
+            }
+            else {
+                i++;
+            }
+        }
+        System.out.println(total);
+    }
 }
